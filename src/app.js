53715cfd6906e9cdd7f505a29ef264a6a328c876
Haseeb <haseebdaone@hotmail.com>
2018-10-17 17:29:11 +0100

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -10,18 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
-
-// add expense water bill
-
-store.dispatch(addExpense({description: 'water bill', amount: 1000}));
-store.dispatch(addExpense({description: 'gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'rent', amount: 22200}));
-
-const state = store.getState()
-getVisibleExpenses(state.expenses, state.filters)
-console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const jsx = (
   <Provider store={store}>
